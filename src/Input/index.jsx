c2942051652b5bddbc0527a2487c45091189aecd
nkrmldrk <jagdm1tr1y@gmail.com>
2023-05-01 19:01:01 +0300

    Initial commit

---
+++ src/Input/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../App.css';
+
+
+export const Input = () => {
+  return (
+    <div className="InputWrap">
+        <input className='Input'/>
+        <button className='Button'>Add city</button>
+    </div>
+  );
+}
