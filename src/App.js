81a87ef8b8f619b4dccb17b823d050039e4fb396
nkrmldrk <jagdm1tr1y@gmail.com>
2023-05-01 19:06:32 +0300

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 import { Input } from './Input';
+import { Card } from './Card';
 
 function App() {
   return (
     <div className="Main">
       <Input/>
+      <Card/>
     </div>
   );
 }
---
+++ src/Card/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import '../App.css';
+
+
+export const Card = () => {
+  return (
+    <div className="Card">
+        
+    </div>
+  );
+}
